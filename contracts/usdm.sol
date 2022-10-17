9906175e77b88fc7d683a4631fb5cf2a53235177
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-17 14:41:32 +0800

    usdm

--- contracts/usdm.sol
+++ contracts/usdm.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract usdm {
-  constructor() public {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDM is ERC20 {
+  constructor(uint256 initialSupply)
+  ERC20("USD Jimao", "USDM") {
+    _mint(msg.sender, initialSupply);
   }
 }
