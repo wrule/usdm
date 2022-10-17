7d9ad6b9f6311a9cec30fb1e2dc4d3e24b4b2192
jimao <jimao@jimaodeMBP.lan>
2022-10-18 00:14:43 +0800

    提交代码

--- contracts/jimao.sol
+++ contracts/jimao.sol
@@ -2,18 +2,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract JIMAO is ERC20 {
-  constructor()
-  ERC20("Jimao coin", "JIMAO") {
-    _mint(msg.sender, 1e18 * 1e8);
-  }
+contract JIMAO {
+  constructor() { }
 
   mapping (address => bool) public minter;
   uint public counter = 0;
 
   function airdrop()
   public payable returns (uint) {
+    address USDC_address = 0x8d667268fA5e0832fa940e03E0297f034ece3f3c;
+    IERC20 coin = IERC20(USDC_address);
     require(minter[msg.sender] != true, "you have already received the airdrop");
     minter[msg.sender] = true;
     counter++;
