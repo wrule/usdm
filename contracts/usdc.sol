49e99133d6e7b595ac3ff35c367f8569322acb75
jimao <jimao@jimaodeMBP.lan>
2022-10-17 23:18:23 +0800

    usdc usdm

--- contracts/usdc.sol
+++ contracts/usdc.sol
@@ -3,9 +3,9 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract USDM is ERC20 {
+contract USDC is ERC20 {
   constructor()
-  ERC20("USDM", "USDM") {
+  ERC20("USDC", "USDC") {
     _mint(msg.sender, 1e20);
   }
 }
