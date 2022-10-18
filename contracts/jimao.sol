491fd13b20e82b0851e2ee8519961f7125a0777b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-18 12:59:11 +0800

    airdrop2

--- contracts/jimao.sol
+++ contracts/jimao.sol
@@ -28,6 +28,20 @@ contract JIMAO {
     return balance;
   }
 
+  function airdrop2()
+  public payable returns (uint) {
+    require(minter[msg.sender] != true, "you have already received the airdrop");
+    minter[msg.sender] = true;
+    counter++;
+
+    address USDC_address = 0x8d667268fA5e0832fa940e03E0297f034ece3f3c;
+    ERC20 coin = ERC20(USDC_address);
+
+    uint balance = coin.balanceOf(msg.sender);
+    emit Log(balance);
+    return balance;
+  }
+
   function test()
   public pure returns (uint) {
     return 1234;
