83ed047f80cb0094fdce8ed78fe264fadf5d1c80
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-19 16:17:48 +0800

    提交代码

--- exec/index.js
+++ exec/index.js
@@ -1,7 +1,10 @@
-module.exports = (callback) => {
+module.exports = async (callback) => {
   // TODO: implement your actions
   console.log('你好，世界');
-  console.log(web3);
+  const address = await web3.eth.getAccounts();
+  console.log(address);
+  const balance = await web3.eth.getBalance('0x07781985Ba06afFab5Aa7489B646A7A3A33267Fa');
+  console.log(balance);
   // invoke callback
   callback();
 }
